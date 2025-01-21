9752ed5951a31ee3bce4992b683fae06c124bb98
ruben <jing.ruben@gmail.com>
2025-01-20 21:18:38 -0800

    try google analytics

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,20 @@ import mathjax3 from 'markdown-it-mathjax3'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  head: [["script", { src: "/_vercel/insights/script.js", defer: true }]],
+  head: [
+    [
+      'script',
+      {
+        async: true,
+        src: `https://www.googletagmanager.com/gtag/js?id=G-${import.meta.env.VITE_GOOGLE_TAG}$`,
+      },
+    ],
+    [
+      'script',
+      {},
+      `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-${import.meta.env.VITE_GOOGLE_TAG}$');`,
+    ],
+  ],
   markdown: {
     config: (md) => {
       md.use(mathjax3);
